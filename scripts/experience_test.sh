3ceebbf817802023bcb5ecd7f6b9793bd69f2ac1
Reed Cozart <Reed.Cozart@ibm.com>
2020-02-27 09:48:23 -0600

    feat: adding experience test per governance standards

---
+++ scripts/experience_test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "Installing Chrome web driver..."
+source <(curl -sSL "${DEVX_SKIT_ASSETS_GIT_URL_RAW:-https://raw.githubusercontent.com/IBM/devex-skit-assets/master}/scripts/install_chrome.sh")
+
+echo "Starting Chrome web driver..."
+source <(curl -sSL "${DEVX_SKIT_ASSETS_GIT_URL_RAW:-https://raw.githubusercontent.com/IBM/devex-skit-assets/master}/scripts/start_chrome.sh")
+
+echo "Downloading and installing pip..."
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+pip -V
+
+echo "Installing Selenium Python package..."
+pip install selenium
+
+echo "Running UI test using Selenium..."
+python3 nodejs_express.py
