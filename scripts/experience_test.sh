a44a33a16dad59b1b3483f209218040000b0337e
Chuck Cox <chuckc@us.ibm.com>
2022-10-19 16:26:10 -0500

    chore: upgrade pip and selenium
    Signed-off-by: chuckc <chuckc@us.ibm.com>

--- scripts/experience_test.sh
+++ scripts/experience_test.sh
@@ -6,10 +6,12 @@ source <(curl -sSL "${DEVX_SKIT_ASSETS_GIT_URL_RAW:-https://github.com/IBM/devex
 echo "Starting Chrome web driver..."
 source <(curl -sSL "${DEVX_SKIT_ASSETS_GIT_URL_RAW:-https://github.com/IBM/devex-skit-assets/raw/v1.2.3}/scripts/start_chrome.sh")
 
-echo "Checking for pip"
+echo "Upgrade pip"
+pip3 install --upgrade pip
 pip3 -V
 
 echo "Installing Selenium Python package..."
+apt-get update
 pip3 install selenium
 
 echo "Running UI test using Selenium..."
