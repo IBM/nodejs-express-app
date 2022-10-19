d0e9c07be9f1c86e32aac7d1cb1f45aa46c2efa5
Chuck Cox <chuckc@us.ibm.com>
2022-10-19 16:06:21 -0500

    chore: upgrade pip
    The current version of pip that this test is using is 9.0.1, and the latest version is 22.3. Upgrading pip should get the latest version of selenium, which is 4.5.

--- scripts/experience_test.sh
+++ scripts/experience_test.sh
@@ -6,7 +6,8 @@ source <(curl -sSL "${DEVX_SKIT_ASSETS_GIT_URL_RAW:-https://github.com/IBM/devex
 echo "Starting Chrome web driver..."
 source <(curl -sSL "${DEVX_SKIT_ASSETS_GIT_URL_RAW:-https://github.com/IBM/devex-skit-assets/raw/v1.2.3}/scripts/start_chrome.sh")
 
-echo "Checking for pip"
+echo "Upgrading pip"
+pip3 install --upgrade pip
 pip3 -V
 
 echo "Installing Selenium Python package..."
